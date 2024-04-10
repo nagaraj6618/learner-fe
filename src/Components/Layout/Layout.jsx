f0ca3a49820a77e3b96f5d1167ba7e8b6ce92d7a
NAGARAJ S <nagaraj516700@gmail.com>
2024-04-10 09:25:02 +0530

    Initial Commit

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+const Layout = () => {
+   return(
+      <div>
+
+         <Header/>
+         <Footer/>
+      </div>
+   )
+};
+
+export default Layout;
