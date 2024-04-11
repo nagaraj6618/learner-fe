65b160b53ef832ba992b56c81b6c7274c0b35c49
NAGARAJ S <nagaraj516700@gmail.com>
2024-04-12 02:57:22 +0530

    Feat : Added Book Component

--- src/Components/Booking/Booked.js
+++ src/Components/Booking/Booked.js
@@ -4,8 +4,8 @@ import { BE_URL, token, userId, userRole } from '../../info/url';
 import './Booked.css'
 const Booked = () => {
   const [booked, setBooked] = useState([]);
+  const [allBooked, setAllBooked] = useState([]);
 
-  
   async function getAllBooked() {
     if (userRole === 'admin' && userRole) {
       const response = await axios.get(`${BE_URL}/booking`, {
@@ -15,6 +15,7 @@ const Booked = () => {
         }
       });
       setBooked(response.data.data);
+      setAllBooked(response.data.data);
       console.table(response.data.data);
     }
     else if (userRole === 'user' && userRole) {
@@ -26,38 +27,90 @@ const Booked = () => {
       });
       console.table(response.data.data);
       setBooked(response.data.data);
+      setAllBooked(response.data.data);
+      
     }
-    
+
 
   }
   useEffect(() => {
     getAllBooked();
+    
   }, []);
 
-  return (
-    <div className='booked-container'>
+  const filterHandler = (e) => {
+    
+    const filterData =  allBooked.filter((data) => data.date === e.target.value);
+    console.table(booked)
+    setBooked(filterData)
+  }
 
-      {
-      (userId &&
-        booked.map((data,index) => (
-        <div key={index} className='booked-container-item'>
-          <div>
-          <h3> {data.name} {data.username}</h3>
-        </div>
-        <div>
-          <p>Time : {`${data.time} ,Date : ${data.date}`}</p>
-        </div>
-        <div>
-          <p>Status : {data.examstatus}</p>
-        </div>
-        <div>
-          <p>Exam type : {data.examtype}</p>
-        </div>
-        </div>))) 
-      
-      }
-      
-      
+  const approveHandler = async(id,status) => {
+    console.table(id,status)
+    try{
+    const response = await axios.put(`${BE_URL}/booking/${id}`,{examstatus:status},
+  {
+    headers:{
+      Authorization:`${token}`
+    }
+  }
+  )
+  console.table(response.data);
+  getAllBooked()
+    }
+    catch(error){
+      alert(error);
+    }
+  }
+  return (
+    <div>
+      <div style={{
+        margin: "10px"
+      }}>Filter
+        <input type='date'
+          style={{
+            padding: "10px",
+            marginLeft: "10px",
+          }}
+          onChange={filterHandler}
+        >
+        </input>
+        <button className='btn btn-login'
+          onClick={() => getAllBooked()}
+        >
+          Clear Filter
+        </button>
+      </div>
+      <div className='booked-container'>
+
+        {
+          (userId &&
+            booked.map((data, index) => (
+              <div key={index} className='booked-container-item'>
+                <div>
+                  <h3> {data.name} {data.username}</h3>
+                </div>
+                <div>
+                  <p>Time : {`${data.time} ,Date : ${data.date}`}</p>
+                </div>
+                <div>
+                  <p>Status : {data.examstatus}</p>
+                </div>
+                <div>
+                  <p>Exam type : {data.examtype}</p>
+                </div>
+                {(userRole === "admin" && <div>
+                  <button className='btn' onClick={() => approveHandler(data._id, 'Approved')}>✔️</button>
+                  <button className='btn' onClick={() => approveHandler(data._id, 'Rejected')}>❌</button>
+                </div>)}
+
+              </div>)))
+
+
+        }
+
+
+      </div>
     </div>
   )
 }
--- src/Components/Booking/SlotBook.js
+++ src/Components/Booking/SlotBook.js
@@ -1,8 +1,88 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Booked.css'
+import { BE_URL, userId, userName,token } from '../../info/url';
+import axios from 'axios';
 const SlotBook = () => {
+  const [examDetails,setExamDetails] = useState(
+    {
+      date:'',
+      time:'',
+      examtype:'CIA',
+      name:userName,
+      username:userId,
+    }
+  );
+
+  const inputHandler = (e) => {
+    setExamDetails(prev=>({...prev, [e.target.id] : e.target.value }))
+  }
+  const formSubmitHandler = async(e)  => {
+    e.preventDefault();
+    console.table(examDetails)
+    try{
+      const response = await axios.post(`${BE_URL}/booking`,examDetails,
+      {
+        withCredentials: true,
+        headers: {
+          Authorization: `${token}`
+        }
+      })
+      console.table(response.data);
+      alert("Solt Booked")
+    }
+    catch(err){
+      alert("Maximum slot reached per day");
+      console.table(err)
+    }
+
+
+  }
   return (
-    <div>SlotBook</div>
+    
+    <div className='slotbook-container'>
+      {(token && (<div>
+         <div className='slotbook-container-item'>
+            <form onSubmit={formSubmitHandler}>
+               <div >
+                <div className='label'>Exam Date</div>
+                   <input
+                     id='date'
+                     type='date'
+                     placeholder='Username'
+                     required
+                     className='input'
+                     onChange={inputHandler}
+                     />
+               </div>
+               
+               <div>
+                <div className='label'>Exam Time</div>
+                  <input
+                     id='time'
+                     type='text'
+                     placeholder='Ex:08:00'
+                     required
+                     className='input'
+                     pattern="([1]?[0-9]|2[0-3]):[0][0]"
+                     onChange={inputHandler}
+                  />
+               </div>
+               
+               <div>
+                <div className='label'>Exam Type</div>
+                <select id='examtype' onChange={inputHandler} className='input'>
+                  <option value='CIA'>CIA</option>
+                  <option value='Module'>Module</option>
+                </select>
+                 
+               </div>
+               <div>
+                  <button className='btn btn-login btn-login-book' type='submit'>Book</button>
+               </div>
+            </form>
+         </div>
+         </div>)) || (<div style={{fontSize:"29px"}}>Login First....</div>)}
+      </div>
   )
 }
 
