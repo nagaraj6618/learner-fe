056b19a5baf473de7c00c96b4ef44d9a7436fbcf
NAGARAJ S <nagaraj516700@gmail.com>
2024-04-12 02:57:40 +0530

    Feat : Added Book Component

--- src/info/url.js
+++ src/info/url.js
@@ -1,5 +1,5 @@
-// const BE_URL = `https://learner-be.vercel.app/api/v1`
-const BE_URL = `http://localhost:8000/api/v1`
+const BE_URL = `https://learner-be.vercel.app/api/v1`
+// const BE_URL = `http://localhost:8000/api/v1`
 const token = localStorage.getItem('token');
 const userId = localStorage.getItem('username');
 const userRole = localStorage.getItem('role')
